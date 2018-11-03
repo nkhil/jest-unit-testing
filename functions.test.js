c5d59a9b7de3fde22d01ade540ac88a98c2299ec
Nikhil Vijayan <nkhilv@gmail.com>
2018-11-03 18:21:31 +0000

    added a few more tests

--- functions.test.js
+++ functions.test.js
@@ -1,7 +1,22 @@
 const functions = require('./functions');
 
 test('Adds 2 + 2 to equal 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
-    
+    expect(functions.add(2, 2)).toBe(4);    
+});
+
+test('Adds 2 + 2 to NOT equal 4', () => {
+    expect(functions.add(2, 2)).not.toBe(5);    
+});
+
+test('Adds 22 + 2 to equal 24', () => {
+    expect(functions.add(22, 2)).toBe(24);    
+});
+
+test('Adds -23 + 23 to NOT equal 1', () => {
+    expect(functions.add(-23, 23)).not.toBe(1);    
+});
+
+test('should be null', () => {
+    expect(functions.isNull()).toBeNull();    
 });
 
